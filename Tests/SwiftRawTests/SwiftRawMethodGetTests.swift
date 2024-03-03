94ba152ea8222013d98113dd2f1c210a2e292b1e
Egor Olegovich <87817946+cbepxbeo@users.noreply.github.com>
2024-03-03 19:31:48 +0300

    Update SwiftRawMethodGetTests.swift
    implementation

--- Tests/SwiftRawTests/SwiftRawMethodGetTests.swift
+++ Tests/SwiftRawTests/SwiftRawMethodGetTests.swift
@@ -22,4 +22,53 @@
 import XCTest
 @testable import SwiftRaw
 
-final class SwiftRawMethodGetTests: XCTestCase {}
+final class SwiftRawMethodGetTests: XCTestCase {
+    func testN1(){
+        let a: Byte = 0x7F //01111111
+        let b: Byte = 0x80 //10000000
+        XCTAssert(a.get(number: .n1) == Bit(.null))
+        XCTAssert(b.get(number: .n1) == Bit(.unit))
+    }
+    func testN2(){
+        let a: Byte = 0xBF //10111111
+        let b: Byte = 0x40 //01000000
+        XCTAssert(a.get(number: .n2) == Bit(.null))
+        XCTAssert(b.get(number: .n2) == Bit(.unit))
+    }
+    func testN3(){
+        let a: Byte = 0xDF //11011111
+        let b: Byte = 0x20 //00100000
+        XCTAssert(a.get(number: .n3) == Bit(.null))
+        XCTAssert(b.get(number: .n3) == Bit(.unit))
+    }
+    func testN4(){
+        let a: Byte = 0xEF //11101111
+        let b: Byte = 0x10 //00010000
+        XCTAssert(a.get(number: .n4) == Bit(.null))
+        XCTAssert(b.get(number: .n4) == Bit(.unit))
+    }
+    func testN5(){
+        let a: Byte = 0xF7 //11110111
+        let b: Byte = 0x08 //00001000
+        XCTAssert(a.get(number: .n5) == Bit(.null))
+        XCTAssert(b.get(number: .n5) == Bit(.unit))
+    }
+    func testN6(){
+        let a: Byte = 0xFB //11111011
+        let b: Byte = 0x04 //00000100
+        XCTAssert(a.get(number: .n6) == Bit(.null))
+        XCTAssert(b.get(number: .n6) == Bit(.unit))
+    }
+    func testN7(){
+        let a: Byte = 0xFD //11111101
+        let b: Byte = 0x02 //00000010
+        XCTAssert(a.get(number: .n7) == Bit(.null))
+        XCTAssert(b.get(number: .n7) == Bit(.unit))
+    }
+    func testN8(){
+        let a: Byte = 0xFE //11111110
+        let b: Byte = 0x01 //00000001
+        XCTAssert(a.get(number: .n8) == Bit(.null))
+        XCTAssert(b.get(number: .n8) == Bit(.unit))
+    }
+}
